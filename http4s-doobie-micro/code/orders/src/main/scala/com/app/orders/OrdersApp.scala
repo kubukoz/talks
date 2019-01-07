f308fdbbcc3afe8101b785203d627b356e83972b
Jakub Kozłowski <kubukoz@gmail.com>
2019-01-07 21:56:18 +0100

    Recover from payment service downtime

--- http4s-doobie-micro/code/orders/src/main/scala/com/app/orders/OrdersApp.scala
+++ http4s-doobie-micro/code/orders/src/main/scala/com/app/orders/OrdersApp.scala
@@ -1,22 +1,21 @@
 package com.app.orders
 
-import cats.data.{EitherT, NonEmptyList, OptionT}
+import cats.data.{EitherT, NonEmptyList}
 import cats.effect._
 import cats.implicits._
-import cats.temp.par.{NonEmptyPar, nonEmptyParToNonEmptyParallel}
-import cats.{Applicative, Monad, MonadError, ~>}
-import com.app.orders.OrderError.{AmountNotPositive, SetNotDivisible, SushiKindNotFound}
+import cats.temp.par.{nonEmptyParToNonEmptyParallel, NonEmptyPar}
+import cats.{~>, Applicative, ApplicativeError, MonadError}
+import com.app.orders.OrderError.{AmountNotPositive, PaymentFailed, SetNotDivisible, SushiKindNotFound}
 import com.app.payments.PaymentMade
 import com.app.sushi.SushiKind
 import io.circe.{Decoder, Encoder}
 import org.http4s.circe._
-import org.http4s.client.{Client, UnexpectedStatus}
 import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.client.{Client, UnexpectedStatus}
 import org.http4s.dsl.Http4sDsl
 import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.{EntityDecoder, EntityEncoder, HttpRoutes, Method, Request, Response, Status, Uri}
-import org.omg.CosNaming.NamingContextPackage.NotFound
+import org.http4s.{EntityDecoder, EntityEncoder, HttpRoutes, Method, Request, Status, Uri}
 
 import scala.concurrent.ExecutionContext
 
@@ -47,14 +46,14 @@ object OrdersModule {
 
   def make[F[_]: Sync: Client: NonEmptyPar]: OrdersModule[F] = {
     type E[A] = EitherT[F, NonEmptyList[OrderError], A]
-    implicit val liftFToE: F ~> E = EitherT.liftK
 
     implicit val sushiClient: SushiClient[E]       = SushiClient.fromClient[F].mapK(EitherT.liftK)
-    implicit val paymentsClient: PaymentsClient[E] = PaymentsClient.fromClient[F].mapK(EitherT.liftK)
-    implicit val orderService: OrderService[E]     = OrderService.make[E]
+    implicit val paymentsClient: PaymentsClient[F] = PaymentsClient.fromClient[F]
+
+    implicit val orderService: OrderService[E] = OrderService.make[E, F]
 
     new OrdersModule[F] {
-      override val routes: HttpRoutes[F] = OrderRoutes.instance[E, F].routes
+      override val routes: HttpRoutes[F] = OrderRoutes.instance[E, F, NonEmptyList[OrderError]].routes
     }
   }
 }
@@ -63,27 +62,19 @@ trait OrderRoutes[F[_]] {
   def routes: HttpRoutes[F]
 }
 
-trait ErrorHandler[E[_], F[_]] {
-  def handle(routes: PartialFunction[Request[F], E[Response[F]]]): HttpRoutes[F]
+trait ErrorHandler[E[_], F[_], A] {
+  def handle[B](eb: E[B])(f: A => F[B]): F[B]
+  def lift[B](fb: F[B]): E[B]
+  def absorb[B](fb: F[Either[A, B]]): E[B]
 }
 
 object ErrorHandler {
-  implicit def handleEitherT[F[_]: Sync, E: Encoder]: ErrorHandler[EitherT[F, E, ?], F] = {
+  implicit def handleEitherT[F[_]: Sync, E]: ErrorHandler[EitherT[F, E, ?], F, E] = {
 
-    new ErrorHandler[EitherT[F, E, ?], F] with Http4sDsl[F] {
-      implicit def entityEncoderForCirce[A: Encoder]: EntityEncoder[F, A] = jsonEncoderOf
-
-      override def handle(routes: PartialFunction[Request[F], EitherT[F, E, Response[F]]]): HttpRoutes[F] =
-        HttpRoutes[F] { request =>
-          OptionT {
-            routes.lift.apply(request).traverse {
-              _.value.flatMap {
-                case Left(e)  => BadRequest(e)
-                case Right(a) => a.pure[F]
-              }
-            }
-          }
-        }
+    new ErrorHandler[EitherT[F, E, ?], F, E] with Http4sDsl[F] {
+      override def handle[B](eb: EitherT[F, E, B])(f: E => F[B]): F[B] = eb.valueOrF(f)
+      override def lift[B](fb: F[B]): EitherT[F, E, B]                 = EitherT.liftF(fb)
+      override def absorb[B](fb: F[Either[E, B]]): EitherT[F, E, B]    = EitherT(fb)
     }
   }
 }
@@ -91,14 +82,16 @@ object ErrorHandler {
 object OrderRoutes {
   implicit def entityEncoderForCirce[F[_]: Applicative, A: Encoder]: EntityEncoder[F, A] = jsonEncoderOf
 
-  def instance[E[_]: OrderService: Sync, F[_]: Monad](implicit handler: ErrorHandler[E, F],
-                                                      liftToE: F ~> E): OrderRoutes[F] =
+  def instance[E[_]: OrderService: Sync, F[_]: Sync, Err: Encoder](
+    implicit handler: ErrorHandler[E, F, Err]): OrderRoutes[F] =
     new OrderRoutes[F] with Http4sDsl[F] {
-      override val routes: HttpRoutes[F] = handler.handle {
+      override val routes: HttpRoutes[F] = HttpRoutes.of[F] {
         case POST -> Root / "order" / sushiKind / IntVar(amount) =>
-          OrderService[E].order(sushiKind, amount).flatMap { totalPrice =>
-            liftToE(Ok(OrderedSushi(amount, sushiKind, totalPrice)))
-          }
+          handler.handle {
+            OrderService[E].order(sushiKind, amount).flatMap { totalPrice =>
+              handler.lift(Ok(OrderedSushi(amount, sushiKind, totalPrice)))
+            }
+          }(BadRequest(_))
       }
     }
 }
@@ -110,7 +103,9 @@ trait OrderService[F[_]] {
 object OrderService {
   def apply[F[_]](implicit F: OrderService[F]): OrderService[F] = F
 
-  def make[F[_]: OrderError.NelMonad: SushiClient: PaymentsClient: NonEmptyPar]: OrderService[F] = new OrderService[F] {
+  def make[F[_]: OrderError.NelMonad: SushiClient: NonEmptyPar,
+           G[_]: PaymentsClient: ApplicativeError[?[_], Throwable]](
+    implicit handler: OrderError.NelErrorHandler[F, G]): OrderService[F] = new OrderService[F] {
 
     def checkKind(kind: String): Option[SushiKind] => F[SushiKind] =
       _.toRight(SushiKindNotFound(kind)).toEitherNel.liftTo[F]
@@ -120,15 +115,21 @@ object OrderService {
 
       if (rem == 0) Applicative[F].unit
       else {
-        val err = SetNotDivisible(requested, kind.setSize, requested - rem)
         OrderError
           .NelMonad[F]
-          .raiseError(err.pure[NonEmptyList])
+          .raiseError(SetNotDivisible(requested, kind.setSize, requested - rem).pure[NonEmptyList])
       }
     }
 
     def checkAmount(requested: Int): F[Unit] = {
       (requested > 0).guard[Option].liftTo[F](NonEmptyList.one(AmountNotPositive(requested)))
+    }
+
+    def makePayment(price: Long): F[PaymentMade] = handler.absorb {
+      PaymentsClient[G]
+        .pay(price)
+        .map(_.asRight[NonEmptyList[OrderError]])
+        .orElse(NonEmptyList.one[OrderError](PaymentFailed).asLeft[PaymentMade].pure[G])
     }
 
     override def order(sushiKind: String, amount: Int): F[Long] = {
@@ -137,7 +138,7 @@ object OrderService {
         .flatMap(checkKind(sushiKind))
         .flatTap(checkSetSize(amount))
         .map(_.price * amount)
-        .flatTap(PaymentsClient[F].pay)
+        .flatTap(makePayment)
 
       (checkAmount(amount), placeOrder).parMapN((_, totalPrice) => totalPrice)
     }
@@ -162,7 +163,7 @@ object SushiClient {
     override def findKind(name: String): F[Option[SushiKind]] =
       client.get(Uri.uri("http://localhost:3000/kinds/by-name/") / name) {
         case resp if resp.status === Status.NotFound => none[SushiKind].pure[F]
-        case resp if resp.status.isSuccess           => resp.decodeJson[SushiKind].map(_.some)
+        case resp if resp.status.isSuccess           => resp.as[SushiKind].map(_.some)
         case resp                                    => Sync[F].raiseError(UnexpectedStatus(resp.status))
       }
   }
@@ -171,9 +172,7 @@ object SushiClient {
 trait PaymentsClient[F[_]] { self =>
   def pay(amount: Long): F[PaymentMade]
 
-  def mapK[G[_]](f: F ~> G): PaymentsClient[G] = new PaymentsClient[G] {
-    override def pay(amount: Long): G[PaymentMade] = f(self.pay(amount))
-  }
+  def mapK[G[_]](f: F ~> G): PaymentsClient[G] = amount => f(self.pay(amount))
 }
 
 object PaymentsClient {
@@ -192,17 +191,20 @@ sealed trait OrderError extends Product with Serializable
 
 object OrderError {
   type NelMonad[F[_]] = MonadError[F, NonEmptyList[OrderError]]
-
   def NelMonad[F[_]](implicit F: NelMonad[F]): NelMonad[F] = F
+
+  type NelErrorHandler[F[_], G[_]] = ErrorHandler[F, G, NonEmptyList[OrderError]]
 
   case class SushiKindNotFound(name: String)                               extends OrderError
   case class SetNotDivisible(requested: Int, setSize: Int, remainder: Int) extends OrderError
   case class AmountNotPositive(requested: Int)                             extends OrderError
+  case object PaymentFailed                                                extends OrderError
 
   val message: OrderError => String = {
     case SushiKindNotFound(name)      => show"Couldn't find sushi kind: $name"
     case AmountNotPositive(requested) => show"The ordered amount must be positive, but it was $requested."
-    case OrderError.SetNotDivisible(requested, setSize, lower) =>
+    case PaymentFailed                => show"Payment failed. Please try again later"
+    case SetNotDivisible(requested, setSize, lower) =>
       val instead = if (lower > 0) show"$lower or ${lower + setSize}" else show"${lower + setSize}"
       show"The requested sushi kind is sold in sets of size $setSize (requested $requested, so maybe try ordering $instead instead?)"
   }
