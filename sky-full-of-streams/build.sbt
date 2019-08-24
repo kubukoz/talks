cf623c29b94c0794510f679d8050782786ceec05
Jakub Kozłowski <kubukoz@gmail.com>
2019-08-24 18:54:18 +0200

    Add JMS examples

--- sky-full-of-streams/build.sbt
+++ sky-full-of-streams/build.sbt
@@ -9,9 +9,13 @@ val commonSettings = Seq(
   scalacOptions ++= Options.all,
   libraryDependencies ++= Seq(
     "org.apache.geronimo.specs" % "geronimo-jms_1.1_spec" % "1.1.1",
+    "org.apache.activemq" % "activemq-client" % "5.15.9",
     "org.typelevel" %% "cats-effect" % "1.4.0",
+    "io.chrisdavenport" %% "log4cats-slf4j" % "0.3.0",
+    "ch.qos.logback" % "logback-classic" % "1.2.3",
     "co.fs2" %% "fs2-io" % "1.0.5",
-    "org.scalacheck" %% "scalacheck"  % "1.13.5",
+    "dev.profunktor" %% "console4cats" % "0.7.0",
+    "org.scalacheck" %% "scalacheck" % "1.13.5",
     "org.scalatest" %% "scalatest" % "3.0.8"
   ) ++ compilerPlugins
 )
--- sky-full-of-streams/src/main/scala/com/example/streams/nonstreaming/JMSListener.scala
+++ sky-full-of-streams/src/main/scala/com/example/streams/nonstreaming/JMSListener.scala
@@ -3,27 +3,58 @@ package com.example.streams.nonstreaming
 import io.circe.Decoder
 import io.circe.parser._
 import javax.jms._
+import org.apache.activemq.ActiveMQConnectionFactory
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
-class JMSConsumer[A: Decoder](consumer: MessageConsumer) {
+class JMSListener(connectionFactory: ConnectionFactory, logger: Logger) {
 
-  def start(handler: A => Unit): Unit =
-    consumer.setMessageListener {
-      case msg: TextMessage =>
-        decode[A](msg.getText()) match {
-          case Left(error)  => throw error
-          case Right(value) => handler(value)
-        }
+  def create[A: Decoder](handler: A => Unit): Unit =
+    withConsumer { consumer =>
+      consumer.setMessageListener {
+        case msg: TextMessage =>
+          decode[A](msg.getText()) match {
+            case Left(error)  => logger.error(s"Decoding error: $error")
+            case Right(value) => handler(value)
+          }
+        case msg => logger.error(s"Not a text message: $msg")
+      }
+      while (true) {}
     }
 
-  def shutdown(): Unit = consumer.close()
+  private def withConsumer[X](f: MessageConsumer => X): X = {
+    val conn = connectionFactory.createConnection()
+    try {
+      conn.start()
+      val ses = conn.createSession(false, javax.jms.Session.AUTO_ACKNOWLEDGE)
+
+      try {
+        ses.run()
+        val consumer = ses.createConsumer(ses.createQueue("demo"))
+        try {
+          f(consumer)
+        } finally {
+          consumer.close()
+        }
+      } finally {
+        ses.close()
+      }
+    } finally {
+      conn.close()
+    }
+
+  }
 }
 
 final case class Event(id: String)
 
-class Demo(consumer: JMSConsumer[Event]) {
+object Demo {
 
-  def run(): Unit =
-    consumer.start { event =>
-      println(event.id)
+  def main(args: Array[String]): Unit = {
+    val cf: ConnectionFactory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616")
+
+    new JMSListener(cf, LoggerFactory.getLogger(classOf[JMSListener])).create[String] { msg =>
+      println(msg)
     }
+  }
 }
