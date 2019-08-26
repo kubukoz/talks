4db6821869c3095dfc467571eb5600094fa897e4
Jakub Kozłowski <kubukoz@gmail.com>
2019-08-26 02:23:50 +0200

    wip

--- sky-full-of-streams/src/main/scala/com/example/streams/Playground.scala
+++ sky-full-of-streams/src/main/scala/com/example/streams/Playground.scala
@@ -4,6 +4,12 @@ import cats.effect._
 
 object Playground extends IOApp {
 
+  import fs2.Stream
+
+  val pureStream = Stream(1, 3, 5)
+
+  val calculatedStream = Stream.iterate(0)(_ + 1)
+
   def run(args: List[String]): IO[ExitCode] =
     ???
 }
